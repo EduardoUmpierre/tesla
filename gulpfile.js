da91cff873259ae8d1728895e3dc258974335462
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2016-07-05 01:34:58 -0300

    Responsive version and optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
  var gulp = require('gulp'),
      sass = require('gulp-sass'),
+     uglify = require('gulp-uglify'),
      concat = require('gulp-concat'),
-     cssnano = require('gulp-cssnano');
+     cssnano = require('gulp-cssnano'),
+     htmlmin = require('gulp-htmlmin');
 
 // SASS
 var css = [
-    './src/css/style.css'
+    './src/css/style.css',
+    './src/css/owl.carousel.css'
 ];
 
 gulp.task('sass', function () {
@@ -14,15 +17,38 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./src/css'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/**/*.scss', ['css']);
-});
-
 gulp.task('css', ['sass'], function () {
   return gulp.src(css)
     .pipe(concat("style.min.css"))
-    // .pipe(cssnano())
+    .pipe(cssnano())
     .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('default', ['sass:watch']);
+// JS
+var js = [
+    './src/js/owl.carousel.js',
+    './src/js/site.js'
+];
+
+gulp.task('js', function() {
+  return gulp.src(js)
+             .pipe(concat('scripts.min.js'))
+             .pipe(uglify())
+             .pipe(gulp.dest('./dist/js'));
+});
+
+// Html
+gulp.task('html', function() {
+  return gulp.src('./src/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+});
+
+// Watch
+gulp.task('watch', function() {
+	gulp.watch('./src/sass/**/*.scss', ['css']);
+    gulp.watch('./src/js/*.js', ['js']);
+    gulp.watch('./src/index.html', ['html']);
+});
+
+gulp.task('default', ['watch']);
